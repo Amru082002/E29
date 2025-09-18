3cd85858009ec67d0af4b5c13f710625d44ed07c
amruta <raskondaamruta32@gamil.com>
2025-09-18 15:06:07 +0530

    commit

---
+++ E29/src/test/java/demo/BatchE29.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class BatchE29 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello guys");
+		System.out.println("hello guys");
+		System.out.println("hello guys");
+		System.out.println("hello guys");
+	}
+
+}
