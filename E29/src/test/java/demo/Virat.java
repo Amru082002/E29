1754d06c3250bbd8163de2e22f1cd1d880e05328
amruta <raskondaamruta32@gamil.com>
2025-09-19 14:20:58 +0530

    virat commit

---
+++ E29/src/test/java/demo/Virat.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Virat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello virat");
+		System.out.println("hello virat");
+		System.out.println("hello virat");
+		System.out.println("hello virat");
+	}
+
+}
